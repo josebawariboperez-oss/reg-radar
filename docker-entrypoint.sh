df7b86cd1434ad289247efbf92459d332cf7823f
josebawariboperez-oss <josebawariboperez@gmail.com>
2025-11-19 20:17:52 +0400

    Track docker entrypoint for Metabase

---
+++ docker-entrypoint.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/sh
+set -e
+
+# Render pone el puerto en $PORT; Metabase usa MB_JETTY_PORT numérico
+if [ -n "$PORT" ]; then
+  export MB_JETTY_PORT="$PORT"
+else
+  export MB_JETTY_PORT="3000"
+fi
+
+# Arrancar Metabase
+exec java -XX:+UseSerialGC -Xmx384m -jar /app/metabase.jar
